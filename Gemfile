4b989f0e0201b99a63615b8b19f77ed92e8b800f
Imogen Hardy <imogen.hardy@gmail.com>
2017-07-04 23:43:34 +0100

    added ability to show random cat gif using giphy api

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source 'https://rubygems.org'
 ruby '2.4.0'
 
 # Use discordrb for Discord API integration
-
 gem 'discordrb'
+
+# Use giphy for Giphy API integration
+gem 'giphy'
--- makkachin.rb
+++ makkachin.rb
@@ -1,4 +1,10 @@
 require "discordrb"
+require "giphy"
+
+Giphy::Configuration.configure do |config|
+  config.version = "v1"
+  config.api_key = "dc6zaTOxFJmzC"
+end
 
 bot = Discordrb::Bot.new token: 'MzMxNTE0MzgzMTA2NzAzMzcx.DD1pyQ.2OKFDwJgIVdH1uqs3VkBqtgjIOM', client_id: 331514383106703371
 
@@ -7,6 +13,7 @@ stringrex = /!sprint in (\d+) for (\d+)/
 bot.mention do |event|
   event.respond "Woof! Here are the things I know how to do: \n
                 - To set up a writing sprint for y minutes in x minutes' time, type \"!sprint in x for y\" \n
+                - To see a cute kitty, type \"!cat\" \n
                 - To ask me for my opinion on steamed buns, type \"!buns\""
 end
 
@@ -21,6 +28,10 @@ end
 
 bot.message(content: "!buns") do |event|
   event.respond "NOM <:makkabuns:331514484378042368>"
+end
+
+bot.message(contains: "!cat") do |event|
+  event.respond "#{Giphy.random('cat').image_url}"
 end
 
 bot.run
